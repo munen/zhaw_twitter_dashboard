7f112815a8c13bd4373221421cbda3da11422a43
Munen Alain M. Lafon <munen@dispatched.ch>
2015-11-24 18:19:59 +0100

    limit concurrent connections to twitter to 2

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const
-express = require('express'),
+var express = require('express'),
 bodyParser = require('body-parser'),
 morgan = require('morgan'),
 app = express(),
@@ -12,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 // Setup
 app.use(morgan('dev'));
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 var twitter_client = new Twitter({
     consumer_key: secrets.consumer_key,
     consumer_secret: secrets.consumer_secret,
@@ -30,12 +29,21 @@ var server = http.createServer(),
 bayeux.attach(server);
 server.listen(8000, function() {
     console.log("faye server ready, captain.");
-})
+});
 // END FAYE
 
-var track_keyword = function(keyword, id) {
+// Twitter limits non-paying customers to two concurrent connections
+var jobs = [{ keyword: "",
+              frequency: 0,
+              stream: undefined },
+            { keyword: "",
+              frequency: 0,
+              stream: undefined }];
+
+var start_tracking = function(keyword, id) {
     twitter_client.stream('statuses/filter', {track: keyword}, function(stream) {
-        console.log("Starting to monitor keyword: '" + keyword);
+        console.log("Starting to monitor keyword: '" + keyword + "' with id '" + id + "'");
+        jobs[id].stream = stream;
         stream.on('data', function(tweet) {
             //tweet.text
             var frequency = ++jobs[id].frequency;
@@ -47,18 +55,27 @@ var track_keyword = function(keyword, id) {
         });
 
         stream.on('error', function(error) {
-            console.log(error);
+            // Possible reasons:
+            //  * Exceeded connection limit for user
+            //  * Easy there, Turbo. Too many requests recently. Enhance your calm.
+            console.log(error.source);
         });
     });
-}
+};
 
-// Twitter limits non-paying customers to two concurrent connections
-var jobs = [
-    { keyword: "",
-      frequency: 0 },
-    { keyword: "",
-      frequency: 0 }
-]
+var track_keyword = function(keyword, id) {
+    // Safeguard: If there's already a monitoring job with the same
+    // id, kill the old stream
+    // https://github.com/desmondmorris/node-twitter/blob/master/lib/twitter.js#L214
+    if(jobs[id].stream) {
+        console.log("Destroying the old stream on a used socket");
+        jobs[id].stream.destroy();
+        setTimeout(start_tracking, 5000, keyword, id);
+    }
+    start_tracking(keyword, id);
+};
+
+
 
 app.put('/keyword_monitor_jobs/:id', jsonParser, function(req, res) {
     if (!req.body || (req.params.id > 2 || req.params.id < 1)) return res.sendStatus(400);
@@ -71,17 +88,17 @@ app.put('/keyword_monitor_jobs/:id', jsonParser, function(req, res) {
     track_keyword(keyword, id);
 
     res.status(201).json({'job': id + 1});
-})
+});
 
 app.get('/twitter_users/:username', function(req, res) {
-    var screen_name = req.params.username
+    var screen_name = req.params.username;
     twitter_client.get("/users/show", { screen_name: screen_name }, function(error, tweets, response) {
         console.log(typeof response);
 
         //res.status(200).json({'follower_count': JSON.parse(response).followers_count});
         res.status(200).json(JSON.parse(response.body));
     });
-})
+});
 
 app.listen(8080, function(){
     console.log("http server ready, captain.");
